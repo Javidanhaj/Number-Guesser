19ec9f4967a3fb77fad081b789451cfd91fcfc06
Javidanhaj <hacizadecavidan.ch@gmail.com>
2024-07-14 03:32:49 +0400

    corrections

--- index.js
+++ index.js
@@ -6,32 +6,41 @@ const label = document.getElementById("mylabel");
 let attempts = 0;
 let guess = 0;
 let isFound = false;
+let stop = false;
 
 document.getElementById('mybutton').addEventListener('click', function() {
     this.style.display = 'none';
-    
+    document.getElementById('info').style.display = 'block';
     while (!isFound) {
-    guess = window.prompt(`Guess a number between ${min} and ${max}`);
-    guess = Number(guess);
-    
-    if(isNaN(guess)) {
-        alert("Please enter a number");
-    }
-    else if(guess < min || guess > max) {
-        alert(`Please enter a number between ${min} and ${max}`);
-    }
-    else{
-        attempts++;
-        if(guess < number) {
-            alert("Too low, try again");
+        guess = window.prompt(`Guess a number between ${min} and ${max}`);
+        if (guess === null) { 
+            alert('Game stopped. Refresh to start again.');
+            break; 
+        } else if (guess.toLowerCase() === 'stop') {
+            alert('Game stopped. Refresh to start again.');
+            break; 
         }
-        else if(guess > number) {
-            alert("Too high, try again");
+        
+        guess = Number(guess);
+        
+        if(isNaN(guess)) {
+            alert("Please enter a number");
         }
-        else {
-            label.textContent = `Congratulations! You guessed the number in ${attempts} attempts`;
-            isFound = true;
+        else if(guess < min || guess > max) {
+            alert(`Please enter a number between ${min} and ${max}`);
         }
-    }
+        else{
+            attempts++;
+            if(guess < number) {
+                alert("Too low, try again");
+            }
+            else if(guess > number) {
+                alert("Too high, try again");
+            }
+            else {
+                label.textContent = `Congratulations! You guessed the number in ${attempts} attempts`;
+                isFound = true;
+            }
+        }
 }
 });
